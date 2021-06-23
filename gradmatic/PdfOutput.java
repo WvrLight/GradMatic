c03f262f1f72d955940f6ca570b503856967f8d1
WvrLight <sSLight22@gmail.com>
2021-06-23 21:22:58 +0800

    reading of files for pdf output clean

--- gradmatic/PdfOutput.java
+++ gradmatic/PdfOutput.java
@@ -34,14 +34,14 @@ public class PdfOutput {
         // File file = new File(DEST);
         // file.getParentFile().mkdirs();
 
-        StudentRecord test = new StudentRecord(122);
-        System.out.println(test.student.studentLN);
-        System.out.println(test.section.sectionName);
-        System.out.println(test.subject.get(0).subjectName);
-        System.out.println(test.grade.get(0).studentPeriodGrade);
-        System.out.println(test.grade.get(1).studentPeriodGrade);
-        System.out.println(test.grade.get(2).studentPeriodGrade);
-        System.out.println(test.grade.get(3).studentPeriodGrade);
+        // StudentRecord test = new StudentRecord(122);
+        // System.out.println(test.student.studentLN);
+        // System.out.println(test.section.sectionName);
+        // System.out.println(test.subject.get(0).subjectName);
+        // System.out.println(test.grade.get(0).studentPeriodGrade);
+        // System.out.println(test.grade.get(1).studentPeriodGrade);
+        // System.out.println(test.grade.get(2).studentPeriodGrade);
+        // System.out.println(test.grade.get(3).studentPeriodGrade);
 
         // new PdfOutput().manipulatePdf(DEST);
     }
@@ -84,8 +84,6 @@ public class PdfOutput {
     private static class StudentRecord {
         Section section = new Section();
         Student student = new Student();
-        // Subject[] subject = null;
-        // Grade[] grade = null;
         ArrayList<Subject> subject = new ArrayList<Subject>();
         ArrayList<Grade> grade = new ArrayList<Grade>();
 
@@ -99,48 +97,32 @@ public class PdfOutput {
                 Statement s = connection.createStatement();
                 s.setQueryTimeout(5);
 
-                // g_count = getCount(s, studentID, "select count(*) as count from Grades where studentID = " + studentID);
-                // System.out.println(g_count);
-                // grade = new Grade[g_count];
-
-                // s_count = getCount(s, studentID, "select count(distinct subjectID) as count from Grades where studentID = " + studentID);
-                // System.out.println(s_count);
-                // subject = new Subject[s_count];
-
                 // Store all student info
                 student = StudentInfo.getStudent(s, studentID);
                 section = SectionInfo.getSection(s, student.sectionID);
 
-
-                // rs = s.executeQuery("select * from Subjects where studentID = " + studentID);
-                // while (rs.next()) {
-                //     subject[loop] = SubjectInfo.getSubject(s, rs.getInt("subjectID"));
-                //     loop++;
-                // }
-
+                // Subjects
                 rs = s.executeQuery("select distinct subjectID from Grades where studentID = " + studentID);
                 while (rs.next()) {
                     subject.add(SubjectInfo.getSubject(s, rs.getInt("subjectID")));
                 }
 
+                // Grades
                 PreparedStatement ps = connection.prepareStatement("select * from Grades where studentID = ? and subjectID = ?");
                 ps.setString(1, "" + studentID);
-                int loop = 0;
                 for (int i = 0; i < subject.size(); i++) {
                     ps.setString(2, "" + subject.get(i).subjectID);
                     rs = ps.executeQuery();
                     for (int j = 1; j <= 4; j++) {
                         if (rs.next()) {
                             grade.add(GradeInfo.getGrade(s, studentID, subject.get(i).subjectID, j));
-                            loop++;
                         }
                     }
                 }
 
                 System.out.println(grade.size());
             }
-            catch (SQLException e)
-            {
+            catch (SQLException e) {
                 // if the error message is "out of memory",
                 // it probably means no database file is found
                 System.err.println(e.getMessage());
@@ -154,23 +136,6 @@ public class PdfOutput {
                     System.err.println(e.getMessage());
                 }
             }
-        }
-
-        static int getCount(Statement s, int studentID, String sql) {
-            int count = 0;
-            try {
-                ResultSet rs = s.executeQuery(sql);
-                if (rs.next()) {
-                    count = rs.getInt("count");
-                }
-            }
-            catch (SQLException e) {
-                // if the error message is "out of memory",
-                // it probably means no database file is found
-                System.err.println(e.getMessage());
-            }
-
-            return count;
         }
     }
 }
